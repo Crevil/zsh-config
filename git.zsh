e875837663c19b222cac6908c0fbbb2b0aad22a2
Bjorn <bjoern@understory.io>
2025-02-03 13:03:05 +0100

    Add generic gcm and gco aliases

--- git.zsh
+++ git.zsh
@@ -2,4 +2,5 @@ alias hb="gh repo view --web"
 alias hc="gh pr create --web"
 alias g="git"
 alias gl="git pull"
-alias gcm="git checkout main"
+alias gcm='git checkout $(git remote show origin | sed -n "/HEAD branch/s/.*: //p")'
+alias gco="git checkout"
