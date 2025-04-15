99a59199ae422d739d2d9b47867fb0fbc4aabb19
Bjorn <bjoern@understory.io>
2025-04-15 09:20:05 +0200

    add GOPATH and aws stuff

--- setup.zsh
+++ setup.zsh
@@ -1,8 +1,15 @@
 export CDPATH="$HOME/understory:$CDPATH"
 export CDPATH="$HOME:$CDPATH"
 
+export GOPATH="$HOME/go"
+export PATH="$GOPATH/bin:$PATH"
+
+export SAVEHIST=10000
+export HISTSIZE=10000
+
 alias l="ls -la"
 alias t="terraform"
 alias editenv="code ~/zsh-config"
 
 source ~/zsh-config/git.zsh
+source ~/zsh-config/aws.zsh
