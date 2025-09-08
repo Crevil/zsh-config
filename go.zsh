e4733dd1d153e87181aac7925ba4eef8ccf82949
Bjorn <bjoern@understory.io>
2025-09-08 10:58:56 +0200

    feat(go): add gt go test function

--- go.zsh
+++ go.zsh
@@ -1,2 +1,10 @@
 export PATH="$(go env GOPATH)/bin:${PATH}"
 export GOPRIVATE="github.com/understory-io"
+
+function gt() {
+    if [ $# -eq 0 ]; then
+        gotestsum --watch ./...
+    else
+        gotestsum "$@"
+    fi
+}
