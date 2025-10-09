3ed3caafec4e395af8abd92776784a35be61db6a
Bjorn <bjoern@understory.io>
2025-10-09 15:05:23 +0200

    ignore comment or empty lines in load_env

--- env.zsh
+++ env.zsh
@@ -1,7 +1,7 @@
 function load_env() {
     local env_file="${1:-.env.local}"
     if [ -f "$env_file" ]; then
-        export $(cat "$env_file" | sed 's/^/export /')
+        export $(cat "$env_file" | grep -v '^#' | grep -v '^$' | sed 's/^/export /')
     else
         echo "Environment file '$env_file' not found"
     fi
