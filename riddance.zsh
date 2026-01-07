30aa09fffab6c484021bad95617edc5bd1ca5ec5
Bjorn <bjoern@understory.io>
2026-01-07 06:47:24 +0100

    full paths for riddance

--- riddance.zsh
+++ riddance.zsh
@@ -1,2 +1,2 @@
-alias riddance-ship-dev="node ../node-aws-env/bin/deploy.js -e development --profile understory-dev -g ../glue/development/glue.json"
-alias riddance-ship-prod="node ../node-aws-env/bin/deploy.js -e production --profile understory-prod -g ../glue/production/glue.json"
+alias riddance-ship-dev="node $HOME/understory/node-aws-env/bin/deploy.js -e development --profile understory-dev -g $HOME/understory/glue/development/glue.json"
+alias riddance-ship-prod="node $HOME/understory/node-aws-env/bin/deploy.js -e production --profile understory-prod -g $HOME/understory/glue/production/glue.json"
